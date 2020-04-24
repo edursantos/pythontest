32b2c979afa6227d74aa66a32086af29116e80dc
Eduardo <eduardo.ramos@discente.univasf.edu.br>
2020-04-24 16:11:18 -0300

    Revert "Revert "Initial commit""
    This reverts commit cdd9d8e9c035cd739a0d03e45f362188f3cbd3d4.

---
+++ ex005.py
@@ -0,0 +1,12 @@
+print('Tabuada')
+n1=int(input('Digite um número qualquer'))
+n2= n1*1
+n3=n1*2
+n4=n1*3
+n5=n1*4
+n6=n1*5
+n7=n1*6
+n8=n1*7
+n9=n1*8
+n10=n1*9
+print('{0} x 1 = {1} \n{0} x 2 = {2} \n{0} x 3 = {3} \n{0} x 4 = {4} \n{0} x 5 = {5} \n{0} x 6 = {6} \n{0} x 7 = {7} \n{0} x 8 = {8} \n{0} x 9 = {9}'.format(n1,n2,n3,n4,n5,n6,n7,n8,n9,n10))
