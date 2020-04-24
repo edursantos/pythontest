759a0b12f16208f9f0659bae26e326409b6c1501
Eduardo <eduardo.ramos@discente.univasf.edu.br>
2020-04-24 16:12:27 -0300

    commit12

--- ex006.py
+++ ex006.py
@@ -2,6 +2,6 @@ print('===Quantos dólares você pode comprar?===')
 n1=float(input('Quanto você tem na carteira?'))
 n2 = n1/5.70
 print('Você tem R$ {:.2f} e pode comprar U${:.2f}'.format(n1,n2))
-print('Teste12')
+print('Teste123')
 
 
